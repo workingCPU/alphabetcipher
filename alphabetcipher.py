6b69c8cb3812bb0293cdb3378c9d61a4a9fa7c6d
aiAdept <crypto_chris@gmx.net>
2024-06-08 15:38:13 -0400

    Third commit

--- alphabetcipher.py
+++ alphabetcipher.py
@@ -30,7 +30,6 @@ class alphaDecrypt() :
 
 			me.args = parser.parse_args()
 
-
 	def _paramToString(me, param: str | os.PathLike) -> str :
 		if isinstance(param, str) :
 			if os.path.isfile(param) : # option file
@@ -39,11 +38,10 @@ class alphaDecrypt() :
 			else : # option text string
 				return str
 
+	_ciph = lambda me, c : c if c != "" else me.cipher
+
 	def analyze(me, cstmCiph = "") :
-		if cstmCiph != "" :
-			cipher = cstmCiph
-		else :
-			cipher = me.cipher # otherwise use default ciph.
+		cipher = me._ciph(cstmCiph)
 
 		fqRef = "ETAOINSHRDLCUMWFGYPBKQVXZ" # English alphabet sorted by fq. 
 		fqCiph = dict(Counter(cipher)) # get freuency of cipher letters.
@@ -67,41 +65,70 @@ class alphaDecrypt() :
 		p = keyN(fqK, fqR) # predict key no.
 		pKey = chr(p) # char of predicted key no.
 
-		dist = p-65 # ascii code to A=1 format.
-
 		if fqK < fqR : # direction bwd IF cipher < clear.
 			dist = dist-26
 
 		print("Most frequent in ciphertext: ", fqKeys[0]) # this cipher.
 		print("Most frequent in cleartext: ", fqRef[x]) # general clear.
 
-		print(f"Predicted key: {pKey} ; Distance: {dist}\n")
+		print(f"Predicted key: {pKey} ; ASCII: {p}\n")
 
-		return [dist, pKey] # pKey: not for mono but poly, e.g.
+		return [p, pKey] # pKey: not for mono but poly, e.g.
+
+
+	graphCount = lambda me, c : Counter(c) # get word count!
+
+
+	def graphs(me, c, n, onlyABC=True) :
+		"""
+		Args :
+			me : 	class-reference.
+			c :	int, ciphertext
+			n :	int, width of output graphs
+			spaces: bool, exclude / include spaces.
+		"""
+
+		if onlyABC :
+			c = c.translate({ord(i): None for i in " ,.()"}) # handle spaces
+		print(c)
+		g = [] # graphs output list.
+		for i in range(2, len(c), 1) :
+			g.append(c[i-n:i])
+
+		return g
+
+
+	def bigraph(me, cstmCiph = "") :
+		cipher = me._ciph(cstmCiph) # get cipher.
+		fqWords = me.graphCount(cipher.split(" "))
+
+		bigraphs = me.graphs(cipher, 2)
+		fqGraphs = me.graphCount(bigraphs)
+
+		print(fqGraphs)
+
+		biList = ["TH", "IN"]
+		distList = []
+		for b in biList :
+			for c in b :
+				# distList.append()
 
 	def trigraph(me, cstmCiph = "") :
-		if cstmCiph == "" :
-			cipher = me.cipher
-		else :
-			cipher = cstmCiph
-
-		wordList = cipher.split(" ") # get word-list.
-		wordCount = Counter(wordList)
-		print(wordCount)
-
+		cipher = me._ciph(cstmCiph)
+		fqWords = me.wordCount(cipher)
 		triList = ["THE"]
 
 
 	# args only for re-usage with poly-alphabetic cipher.
 	def decrypt(me, char, dist) :
-		clearC = lambda c, k : chr((ord(c) - k - 65) % 26 + 65)  # clear Char.
+		clearC = lambda c, k : chr((ord(c) - k) % 26 + 65)  # clear Char.
 
 		return clearC(char, dist)
 
 	def decryptMono(me) :
 		clear = "" # clear text to be.
 
-		dist = me.analyze(me.cipher)[0] # get distance / predicted shift.
+		dist = me.analyze(me.cipher)[0]  # get distance / predicted shift.
 
 		for c in me.cipher :
 			if ord(c) in range(65, 91) :
@@ -140,7 +167,7 @@ class alphaDecrypt() :
 				clear += c
 
 		print(clear)
-		triRes = me.trigraph() # no args, just testing.
+		triRes = me.bigraph() # no args, just testing.
 
 def main() :
 	a = alphaDecrypt()
